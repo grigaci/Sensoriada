1f7399605bcd6c00dd8f6ad148a20de89c8397f4
Bogdan Iusco <bogdan.iusco@gmail.com>
2015-01-09 23:18:39 +0200

    Add base project.

---
+++ SensoriadaExample/Classes/SSAppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SSAppDelegate.swift
+//  SensoriadaExample
+//
+//  Created by bogdan on 09/01/15.
+//  Copyright (c) 2015 Grigaci. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class SSAppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
