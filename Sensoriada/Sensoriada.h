bb06d50be3698df6668bca0d462878d9811cb8cb
Bogdan Iusco <bogdan.iusco@gmail.com>
2015-01-10 11:28:31 +0200

    Add Sensoriada framework target.

---
+++ Sensoriada/Sensoriada.h
@@ -0,0 +1,19 @@
+//
+//  Sensoriada.h
+//  Sensoriada
+//
+//  Created by bogdan on 10/01/15.
+//  Copyright (c) 2015 Grigaci. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Sensoriada.
+FOUNDATION_EXPORT double SensoriadaVersionNumber;
+
+//! Project version string for Sensoriada.
+FOUNDATION_EXPORT const unsigned char SensoriadaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sensoriada/PublicHeader.h>
+
+
